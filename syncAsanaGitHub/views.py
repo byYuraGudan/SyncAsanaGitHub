c5358b9845a86e0391bd6a975ac55d2a72c6d0cd
Yura Gudan <yura.gudan@gmail.com>
2019-06-14 21:54:59 +0300

    Modfied views to post request

--- syncAsanaGitHub/views.py
+++ syncAsanaGitHub/views.py
@@ -1,21 +1,29 @@
-from django.shortcuts import render
 import hmac
 from hashlib import sha1
 
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.utils.encoding import force_bytes
 
 import requests
 from ipaddress import ip_address, ip_network
 
-# Create your views here.
-
+@require_POST
 @csrf_exempt
 def hello(request):
     # Verify if request came from GitHub
-    # ...
+    forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
+    client_ip_address = ip_address(forwarded_for)
+    whitelist = requests.get('https://api.github.com/meta').json()['hooks']
+
+    for valid_ip in whitelist:
+        if client_ip_address in ip_network(valid_ip):
+            break
+    else:
+        return HttpResponseForbidden('Permission denied.')
+
     # Verify the request signature
     header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
     if header_signature is None:
@@ -30,7 +38,18 @@ def hello(request):
         return HttpResponseForbidden('Permission denied.')
 
     # If request reached this point we are in a good shape
-    return HttpResponse('pong')
+    # Process the GitHub events
+    event = request.META.get('HTTP_X_GITHUB_EVENT', 'ping')
+
+    if event == 'ping':
+        return HttpResponse('pong')
+    elif event == 'push':
+        # Deploy some code for example
+        return HttpResponse('success')
+
+    # In case we receive an event that's not ping or push
+    return HttpResponse(status=204)
+
 
 def index(request):
     return  HttpResponse('Hello, world. You`are at the sync index')
