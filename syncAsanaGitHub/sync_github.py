658d61348eeb78b5dc2513eda00d3956b0182fc2
Yura Gudan <yura.gudan@gmail.com>
2019-07-01 00:58:50 +0300

    Asignee and labels with asana

--- syncAsanaGitHub/sync_github.py
+++ syncAsanaGitHub/sync_github.py
@@ -21,8 +21,27 @@ if 'GITHUB_ACCESS_TOKEN' in os.environ:
     gitClient = Github(os.environ['GITHUB_ACCESS_TOKEN'])
     repoGit = gitClient.get_repo("byYuraGudan/SyncAsanaGitHub")
 
+def checking_request(events):
+    try:
+        request_logger.info(events)
+        for event in events:
+            print(event)
+            if event.get('parent') != 1127318215881837:
+                if event.get('type') == 'task':
+                    if event.get('action') == 'added':
+                        request_logger.info('create task')
+                        return create_task(event)
+                    if event.get('action') == 'changed':
+                        return change_task(event)
+                    if event.get('action') == 'deleted':
+                        return delete_task(event)
 
-def create_issue(event):
+    except AttributeError as err:
+        request_logger.info("Error - %s"%err)
+        return HttpResponse("Error", status=500)
+    return HttpResponse("OK")
+
+def create_task(event):
     if len(list(IdentityID.objects.filter(asana_id=event.get('resource')))) > 0:
         return HttpResponse("OK",status=200)
     else:
@@ -34,7 +53,7 @@ def create_issue(event):
         return HttpResponse("Create issue",status=201)
 
 
-def change_issue(event):
+def change_task(event):
     task = asanaClient.tasks.find_by_id(event.get('resource'))
     github_task_number = list(IdentityID.objects.filter(asana_id = task.get('id')))
     if len(github_task_number) > 0:
@@ -52,35 +71,18 @@ def change_issue(event):
         request_logger.info("Issue not changed")
         return HttpResponse("Issue changed", status=304)
 
+def delete_task(event):
+    request_logger.info('Delete task')
+    IdentityID.objects.filter(github_id=event.get('resource')).delete()
+    return HttpResponse('Delete task',status=204)
 
 
 
-
-
-def checking_request(events):
-    try:
-        request_logger.info(events)
-        if len(events) > 0:
-            for event in events:
-                print(event)
-                if event.get('parent') != 1127318215881837:
-                    if event.get('type') == 'task':
-                        if event.get('action') == 'added':
-                            request_logger.info('create task')
-                            return create_issue(event)
-                        if event.get('action') == 'changed':
-                            return change_issue(event)
-                        if event.get('action') == 'deleted':
-                            pass
-    except AttributeError as err:
-        request_logger.info("Error - %s"%err)
-        return HttpResponse("Error", status=500)
-    return HttpResponse("OK")
 
 
 def task_event(event):
     if event.action == 'added':
-        create_issue(event)
+        create_task(event)
     if event.action == 'changed':
         pass
 
